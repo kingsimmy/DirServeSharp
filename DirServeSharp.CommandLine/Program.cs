37960e1eaa4a80307d19ad7a938c8a866a224594
Simran Barn <kingsimmy@users.noreply.github.com>
2017-04-12 21:22:50 +0100

    Initial commit

--- DirServeSharp.CommandLine/Program.cs
+++ DirServeSharp.CommandLine/Program.cs
@@ -1,12 +1,37 @@
 ﻿using System;
+using System.IO;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
 
 namespace DirServeSharp.CommandLine
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string staticFileDirectory;
+            if (args.Length > 0)
+            {
+                staticFileDirectory = args[0];
+            }
+            else
+            {
+                Console.WriteLine("Enter the directory to serve:");
+                staticFileDirectory = Console.ReadLine();
+            }
+            if (!Directory.Exists(staticFileDirectory))
+            {
+                Console.WriteLine($"'{staticFileDirectory}' does not exist");
+                Environment.Exit(1);
+            }
+            IWebHost host = new WebHostBuilder()
+                .UseUrls("http://*:8080")
+                .UseContentRoot(staticFileDirectory)
+                .UseWebRoot(staticFileDirectory)
+                .Configure(appbuilder => appbuilder.UseFileServer(true).Build())
+                .UseKestrel()
+                .Build();
+            host.Run();
         }
     }
 }
