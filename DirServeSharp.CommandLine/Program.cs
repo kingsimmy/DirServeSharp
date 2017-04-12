8842b411335fbf271b6d0724339da8c0e55a06e4
Simran Barn <kingsimmy@users.noreply.github.com>
2017-04-12 20:35:58 +0100

    Add project files.

---
+++ DirServeSharp.CommandLine/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DirServeSharp.CommandLine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
